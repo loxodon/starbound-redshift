2c188c45d2fe45eb275054cf8265e636b25dbca4
loxodon <locuturus.dev@gmail.com>
2021-03-28 14:53:55 +0700

    feat(EventHandler): upgrade to be compatible with IFTTT

--- src/event-handler/EventHandler.ts
+++ src/event-handler/EventHandler.ts
@@ -1,18 +1,19 @@
-import { IEventHandler } from './EventHandler.interface'
-import { IHttpRequestService } from '../http/HttpRequestService.interface'
+import { IEventHandler } from "./EventHandler.interface"
+import { IHttpRequestService } from "../http/HttpRequestService.interface"
 
 export class EventHandler implements IEventHandler {
   httpRequestService: IHttpRequestService
 
-  public constructor(
-    httpRequestService: IHttpRequestService
-  ) {
+  public constructor(httpRequestService: IHttpRequestService) {
     this.httpRequestService = httpRequestService
   }
 
   public handleEvent(eventName: string, eventArgs: any): Promise<any> {
+    let endpoint = process.env.api_default_endpoint
+    endpoint = endpoint.replace("{event_name}", eventName)
+    endpoint = endpoint.replace("{api_key}", process.env.api_key)
     return this.httpRequestService.post(
-      `${process.env.api_url}/${process.env.api_default_endpoint}`,
+      `${process.env.api_url}/${endpoint}`,
       this.buildRequestData(eventName, eventArgs),
       this.buildRequestConfig()
     )
@@ -23,6 +24,6 @@ export class EventHandler implements IEventHandler {
   }
 
   private buildRequestConfig() {
-    return { headers: { "X-API-KEY": process.env.api_key }}
+    return { headers: { "X-API-KEY": process.env.api_key } }
   }
 }
