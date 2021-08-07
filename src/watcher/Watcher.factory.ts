59b4e8d3f8f35ed20c907c5d340516da078c63c1
Locuturus <locuturus.dev@gmail.com>
2021-08-07 22:25:39 +0700

    fix bug

--- src/watcher/Watcher.factory.ts
+++ src/watcher/Watcher.factory.ts
@@ -2,7 +2,7 @@ import { Watcher } from "./Watcher"
 
 export class WatcherFactory {
   public static make() {
-    const filePath = process.env.starbound_log_file_path
+    const filePath = process.env.STARBOUND_LOG_FILE_PATH
     return new Watcher(filePath)
   }
 }
